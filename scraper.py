aec94def61c04e79ffdd3816a925ec2ae1d3b11a
Samuel Soto <samuelsotodam20@iescastelar.com>
2022-08-18 06:49:12 +0200

    Added case in which a player will not be part of the league ("playerStatus").
    It caused an error in the script that stopped its execution when reaching the player with ID 200

--- scraper.py
+++ scraper.py
@@ -91,57 +91,58 @@ def main(argv=None):
             response = requests.get('%s/%s' % (players_endpoint,index))
             if response.status_code == 200:
                 payload = response.json()
-                team_id = payload['team']['id']
-                team_shortname = payload['team']['shortName']
-                team_folder = 'laliga/%s-%s' % (team_id, team_shortname)
-                if not os.path.exists(team_folder):
-                    os.makedirs(team_folder)
-                player_id = index
-                player_slug = payload['slug']
-                player_filename = 'laliga/%s-%s/%s-%s.json' % (team_id, team_shortname, player_id, player_slug)
-                player_status = payload['playerStatus']
-                if player_status != 'out_of_league':
-                    f = open(player_filename, 'w', encoding='utf-8')
-                    json.dump(payload, f, indent=4)
-                    f.close()                
-                    logger_.info('Scraped player with id=%s' % index)
-                    player_scores = [ 0 for x in range(0,38)]
-                    player_scores_aggr = [ 0 for x in range(0,38)]
-                    player_aggr = 0
-                    for week in payload['playerStats']:
-                        stats = week['stats']
-                        week_number = week['weekNumber']
-                        total_points = week['totalPoints']
-                        csv_row = '%s,%s,%s,%s,%s' % (player_id,player_slug,team_shortname,week_number,total_points)
-                        csv_row += ',%s,%s,%s,%s' % (stats['mins_played'][0],stats['goals'][0],stats['goal_assist'][0],stats['offtarget_att_assist'][0])
-                        csv_row += ',%s,%s,%s,%s' % (stats['pen_area_entries'][0],stats['penalty_won'][0],stats['penalty_save'][0],stats['saves'][0])
-                        csv_row += ',%s,%s,%s,%s' % (stats['effective_clearance'][0],stats['penalty_failed'][0],stats['own_goals'][0],stats['goals_conceded'][0])
-                        csv_row += ',%s,%s,%s,%s' % (stats['yellow_card'][0],stats['second_yellow_card'][0],stats['red_card'][0],stats['total_scoring_att'][0])
-                        csv_row += ',%s,%s,%s,%s' % (stats['won_contest'][0],stats['ball_recovery'][0],stats['poss_lost_all'][0],stats['marca_points'][0])
-                        csv_row += ',%s,%s,%s,%s' % (stats['mins_played'][1],stats['goals'][1],stats['goal_assist'][1],stats['offtarget_att_assist'][1])
-                        csv_row += ',%s,%s,%s,%s' % (stats['pen_area_entries'][1],stats['penalty_won'][1],stats['penalty_save'][1],stats['saves'][1])
-                        csv_row += ',%s,%s,%s,%s' % (stats['effective_clearance'][1],stats['penalty_failed'][1],stats['own_goals'][1],stats['goals_conceded'][1])
-                        csv_row += ',%s,%s,%s,%s' % (stats['yellow_card'][1],stats['second_yellow_card'][1],stats['red_card'][1],stats['total_scoring_att'][1])
-                        csv_row += ',%s,%s,%s,%s' % (stats['won_contest'][1],stats['ball_recovery'][1],stats['poss_lost_all'][1],stats['marca_points'][1])
-                        csv_content.append(csv_row)
-                        player_aggr += total_points
-                        player_scores[week_number - 1] = total_points               
-                        player_scores_aggr[week_number - 1] = player_aggr               
-                    player_csv_row = '%s,%s,%s,%s' % (player_id,team_shortname,payload['points'],payload['averagePoints'])
-                    player_csv_row += ',"%s","%s",%s,%s' % (payload['name'],payload['nickname'],payload['slug'],payload['positionId'])
-                    player_csv_row += ',%s,%s,%s' % (payload['position'],payload['marketValue'],payload['playerStatus'])
-                    for w in range(0,38):
-                        player_csv_row += ',%s' % player_scores[w]
-                    old_player_score = 0
-                    for w in range(0,38):
-                        new_player_score = old_player_score
-                        if player_scores_aggr[w] > 0:
-                            new_player_score = player_scores_aggr[w]
-                            old_player_score = new_player_score
-                        player_csv_row += ',%s' % new_player_score
-                    player_aggr_content.append(player_csv_row)
-                else:
-                    logger_.warning('Player with id=%s not in LaLiga anymore' % index)
+                if payload['playerStatus'] == "ok":
+                    team_id = payload['team']['id']
+                    team_shortname = payload['team']['shortName']
+                    team_folder = 'laliga/%s-%s' % (team_id, team_shortname)
+                    if not os.path.exists(team_folder):
+                        os.makedirs(team_folder)
+                    player_id = index
+                    player_slug = payload['slug']
+                    player_filename = 'laliga/%s-%s/%s-%s.json' % (team_id, team_shortname, player_id, player_slug)
+                    player_status = payload['playerStatus']
+                    if player_status != 'out_of_league':
+                        f = open(player_filename, 'w', encoding='utf-8')
+                        json.dump(payload, f, indent=4)
+                        f.close()                
+                        logger_.info('Scraped player with id=%s' % index)
+                        player_scores = [ 0 for x in range(0,38)]
+                        player_scores_aggr = [ 0 for x in range(0,38)]
+                        player_aggr = 0
+                        for week in payload['playerStats']:
+                            stats = week['stats']
+                            week_number = week['weekNumber']
+                            total_points = week['totalPoints']
+                            csv_row = '%s,%s,%s,%s,%s' % (player_id,player_slug,team_shortname,week_number,total_points)
+                            csv_row += ',%s,%s,%s,%s' % (stats['mins_played'][0],stats['goals'][0],stats['goal_assist'][0],stats['offtarget_att_assist'][0])
+                            csv_row += ',%s,%s,%s,%s' % (stats['pen_area_entries'][0],stats['penalty_won'][0],stats['penalty_save'][0],stats['saves'][0])
+                            csv_row += ',%s,%s,%s,%s' % (stats['effective_clearance'][0],stats['penalty_failed'][0],stats['own_goals'][0],stats['goals_conceded'][0])
+                            csv_row += ',%s,%s,%s,%s' % (stats['yellow_card'][0],stats['second_yellow_card'][0],stats['red_card'][0],stats['total_scoring_att'][0])
+                            csv_row += ',%s,%s,%s,%s' % (stats['won_contest'][0],stats['ball_recovery'][0],stats['poss_lost_all'][0],stats['marca_points'][0])
+                            csv_row += ',%s,%s,%s,%s' % (stats['mins_played'][1],stats['goals'][1],stats['goal_assist'][1],stats['offtarget_att_assist'][1])
+                            csv_row += ',%s,%s,%s,%s' % (stats['pen_area_entries'][1],stats['penalty_won'][1],stats['penalty_save'][1],stats['saves'][1])
+                            csv_row += ',%s,%s,%s,%s' % (stats['effective_clearance'][1],stats['penalty_failed'][1],stats['own_goals'][1],stats['goals_conceded'][1])
+                            csv_row += ',%s,%s,%s,%s' % (stats['yellow_card'][1],stats['second_yellow_card'][1],stats['red_card'][1],stats['total_scoring_att'][1])
+                            csv_row += ',%s,%s,%s,%s' % (stats['won_contest'][1],stats['ball_recovery'][1],stats['poss_lost_all'][1],stats['marca_points'][1])
+                            csv_content.append(csv_row)
+                            player_aggr += total_points
+                            player_scores[week_number - 1] = total_points               
+                            player_scores_aggr[week_number - 1] = player_aggr               
+                        player_csv_row = '%s,%s,%s,%s' % (player_id,team_shortname,payload['points'],payload['averagePoints'])
+                        player_csv_row += ',"%s","%s",%s,%s' % (payload['name'],payload['nickname'],payload['slug'],payload['positionId'])
+                        player_csv_row += ',%s,%s,%s' % (payload['position'],payload['marketValue'],payload['playerStatus'])
+                        for w in range(0,38):
+                            player_csv_row += ',%s' % player_scores[w]
+                        old_player_score = 0
+                        for w in range(0,38):
+                            new_player_score = old_player_score
+                            if player_scores_aggr[w] > 0:
+                                new_player_score = player_scores_aggr[w]
+                                old_player_score = new_player_score
+                            player_csv_row += ',%s' % new_player_score
+                        player_aggr_content.append(player_csv_row)
+                    else:
+                        logger_.warning('Player with id=%s not in LaLiga anymore' % index)
             if response.status_code == 404:
                 logger_.warn('Player with id=%s not found' % index)
         f = open('laliga/players-performance.csv', 'w', encoding='utf-8')
